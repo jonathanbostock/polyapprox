b7ae2b73df754cadb14db9528101e327c20c2d93
Nora Belrose <belrose.nora@gmail.com>
2024-10-24 15:38:57 -0700

    Compute mean output of GLUs

--- polyapprox/ols.py
+++ polyapprox/ols.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from functools import partial
 
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike, NDArray
 import numpy as np
 
 from .extra import sigmoid, sigmoid_prime, swish, swish_prime
@@ -11,6 +11,24 @@ from .relu import relu_ev, relu_prime_ev
 
 
 OlsResult = namedtuple('OlsResult', ['alpha', 'beta'])
+
+
+# Mapping from activation functions to EVs
+ACT_TO_EVS = {
+    'gelu': gelu_ev,
+    'relu': relu_ev,
+    'sigmoid': partial(gauss_hermite, sigmoid),
+    'swish': partial(gauss_hermite, swish),
+    'tanh': partial(gauss_hermite, np.tanh),
+}
+# Mapping from activation functions to EVs of their derivatives
+ACT_TO_PRIME_EVS = {
+    'gelu': gelu_prime_ev,
+    'relu': relu_prime_ev,
+    'sigmoid': partial(gauss_hermite, sigmoid_prime),
+    'swish': partial(gauss_hermite, swish_prime),
+    'tanh': partial(gauss_hermite, lambda x: 1 - np.tanh(x)**2),
+}
 
 
 def ols(
@@ -48,24 +66,11 @@ def ols(
     if mean is not None:
         preact_mean += W1 @ mean
 
-    match act:
-        case 'gelu':
-            act_ev = gelu_ev
-            act_prime_ev = gelu_prime_ev
-        case 'relu':
-            act_ev = relu_ev
-            act_prime_ev = relu_prime_ev
-        case 'sigmoid':
-            act_ev = partial(gauss_hermite, sigmoid)
-            act_prime_ev = partial(gauss_hermite, sigmoid_prime)
-        case 'swish':
-            act_ev = partial(gauss_hermite, swish)
-            act_prime_ev = partial(gauss_hermite, swish_prime)
-        case 'tanh':
-            act_ev = partial(gauss_hermite, np.tanh)
-            act_prime_ev = partial(gauss_hermite, lambda x: 1 - np.tanh(x)**2)
-        case _:
-            raise ValueError(f"Unknown activation function: {act}")
+    try:
+        act_ev = ACT_TO_EVS[act]
+        act_prime_ev = ACT_TO_PRIME_EVS[act]
+    except KeyError:
+        raise ValueError(f"Unknown activation function: {act}")
 
     # Apply Stein's lemma to compute cross-covariance of the input
     # with the activations. We need the expected derivative of the
@@ -86,3 +91,55 @@ def ols(
         alpha -= beta.T @ mean
 
     return OlsResult(alpha, beta)
+
+
+def glu_mean(
+    W: NDArray,
+    V: NDArray,
+    b1: ArrayLike = 0.0,
+    b2: ArrayLike = 0.0,
+    *,
+    act: str = 'sigmoid',
+    mean: NDArray | None = None,
+    cov: NDArray | None = None,
+):
+    """Analytically compute the mean output of a gated linear unit (GLU).
+
+    See "GLU Variants Improve Transformer" <https://arxiv.org/abs/2002.05202>
+    by Shazeer (2020) for more details.
+    """
+    # The network takes the form σ(W @ x + b1) * (V @ x + b2)
+    # Let y = W @ x + b1 and z = V @ x + b2
+    if cov is not None:
+        # Cross-covariance matrix of y and z
+        cross_cov = W @ cov @ V.T
+
+        y_std = np.diag(W @ cov @ W.T) ** 0.5
+        # z_std = np.diag(V @ cov @ V.T) ** 0.5
+    else:
+        cross_cov = W @ V.T
+        y_std = np.linalg.norm(W, axis=1)
+        # z_std = np.linalg.norm(V, axis=1)
+
+    y_mean = np.array(b1)
+    if mean is not None:
+        y_mean += W @ mean
+
+    z_mean = np.array(b2)
+    if mean is not None:
+        z_mean += V @ mean
+    
+    try:
+        act_ev = ACT_TO_EVS[act]
+        act_prime_ev = ACT_TO_PRIME_EVS[act]
+    except KeyError:
+        raise ValueError(f"Unknown activation function: {act}")
+    
+    # Apply Stein's lemma to compute
+    # E[GLU(x)]_i = E[σ(y_i) * z_i] = Cov(σ(y_i), z_i) + E[σ(y_i)] * E[z_i]
+    # The lemma says that Cov(y_i, σ(z_i)) = Cov(y_i, z_i) * E[σ'(y_i)]
+    # so we need to compute E[σ'(y_i)] for each i
+    act_mean = act_ev(y_mean, y_std)
+    output_mean = np.diag(cross_cov) * act_prime_ev(y_mean, y_std) + act_mean * z_mean
+
+    return output_mean
