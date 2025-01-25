10fa8a0e146a2fc4c2fc021b2694d8ca531c4c5e
Nora Belrose <belrose.nora@gmail.com>
2025-01-25 13:23:38 -0800

    Monte Carlo test with statsmodels

--- tests/test_ols.py
+++ tests/test_ols.py
@@ -1,6 +1,15 @@
 import numpy as np
+import pytest
+import statsmodels.api as sm
+from scipy.stats import multivariate_normal as mvn
+from statsmodels.regression.linear_model import OLS
 
+from polyapprox.gelu import gelu
 from polyapprox.ols import ols
+
+
+def relu(x):
+    return np.maximum(0, x)
 
 
 def test_ols_relu():
@@ -20,7 +29,7 @@ def test_ols_relu():
     # Monte Carlo check that the FVU is below 1
     n = 10_000
     x = np.random.randn(n, d)
-    y = np.maximum(0, x @ W1.T) @ W2.T
+    y = relu(x @ W1.T) @ W2.T
 
     # Quadratic FVU should be lower than linear FVU, which should be lower than 1
     lin_fvu = np.square(y - lin_res(x)).sum() / np.square(y).sum()
@@ -37,3 +46,52 @@ def test_ols_relu():
     np.testing.assert_allclose(lin_res.alpha, 0)
     np.testing.assert_allclose(quad_res.alpha, 0)
     np.testing.assert_allclose(quad_res.gamma, 0)
+
+
+@pytest.mark.parametrize("act", ["gelu", "relu"])
+@pytest.mark.parametrize("k", [1, 2, 3])
+def test_ols_monte_carlo(act: str, k: int):
+    # Determinism
+    np.random.seed(0)
+
+    # Choose activation function
+    act_fn = gelu if act == "gelu" else relu
+
+    # Implement the MLP
+    def mlp(x, W1, b1, W2, b2):
+        return act_fn(x @ W1.T + b1) @ W2.T + b2
+
+    d_in = 10
+    d_inner = 1_000
+    d_out = 1
+
+    # Construct a random Gaussian mixture with k components
+    # random psd matrix d_in x d_in
+    A = np.random.randn(k, d_in, d_in) / np.sqrt(d_in)
+    cov_x = A @ A.mT
+    mu_x = np.random.randn(k, d_in)
+
+    W1 = np.random.randn(d_inner, d_in) / np.sqrt(d_in)
+    W2 = np.random.randn(d_out, d_inner) / np.sqrt(d_inner)
+    b1 = np.random.randn(d_inner) / np.sqrt(d_in)
+    b2 = np.random.randn(d_out)
+
+    # Compute analytic coefficients
+    analytic = ols(W1, b1, W2, b2, act=act, cov=cov_x.squeeze(), mean=mu_x.squeeze())
+
+    # Generate Monte Carlo data
+    x = np.concatenate(
+        [mvn.rvs(mean=mu_x[i], cov=cov_x[i], size=10_000) for i in range(k)]
+    )
+    y = mlp(x, W1, b1, W2, b2)
+
+    # Use statsmodels to approximate the coefficients
+    X = sm.add_constant(x)
+    empirical = OLS(y.squeeze(), X).fit()
+
+    # Check that analytic coefficients are within the confidence interval
+    lo, hi = empirical.conf_int(0.01).T
+    analytic_beta = analytic.beta.squeeze()
+
+    assert lo[0] < analytic.alpha < hi[0]
+    assert np.all((lo[1:] < analytic_beta) & (analytic_beta < hi[1:]))
