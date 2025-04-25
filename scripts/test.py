6fb609f3138e3600e774ace18b38cfdfa1dc40a1
jb <jonathan.d.bostock@gmail.com>
2025-04-25 18:52:07 +0100

    Definintely working for some thigns now/home/jb/Documents/polyapprox/venv/bin/python

--- scripts/test.py
+++ scripts/test.py
@@ -9,7 +9,7 @@ def main():
     W_2 = torch.randn(5, 20)
     b_2 = torch.randn(5)
 
-    ols_result = ols(W_1, b_1, W_2, b_2, order="quadratic")
+    ols_result = ols(W_1, b_1, W_2, b_2, activation="gelu", order="quadratic")
     print(ols_result)
 
 
