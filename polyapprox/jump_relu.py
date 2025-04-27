97c2b672d32575099be4fee50afa50f0602e6f4c
jb <jonathan.d.bostock@gmail.com>
2025-04-27 14:53:38 +0100

    Added ImportWarning to jump_relu

--- polyapprox/jump_relu.py
+++ polyapprox/jump_relu.py
@@ -5,7 +5,16 @@ from scipy.special import factorial2
 
 from .backends import ArrayType, norm_cdf, norm_pdf
 
-# All of these functions assume theta = 1.0, which we can switch everything to be like
+raise ImportWarning(
+    """
+    JumpReLU currently fails certain tests of OLS regression, though the values are typically close.
+    I don't know if this is an issue with the OLS implementation or the JumpReLU itself.
+    Use at your own risk.
+    """
+)
+
+# All of these functions assume theta = 1.0, which loses no generality because
+# we can always rescale our weights and biases to get an equivalent network.
 
 def jump_relu_ev(mu: ArrayType, sigma: ArrayType) -> ArrayType:
     """Expected value of JumpReLU(x) under N(mu, sigma)"""
